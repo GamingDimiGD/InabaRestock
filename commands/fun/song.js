8b4769e9f46928fad842d856d2a74d2adda8cec5
GamingDimiGD <gamingdimigd@gmail.com>
2026-08-08 08:55:40 +0300

    Finishing (?) touches

--- commands/fun/song.js
+++ commands/fun/song.js
@@ -14,12 +14,14 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply();
         const songs = JSON.parse(fs.readFileSync("./songs.json", "utf-8"));
-        let song = songs[interaction.options.getInteger("number") - 1] || songs[Math.floor(Math.random() * songs.length)];
+        const number = interaction.options.getInteger("number") || Math.floor(Math.random() * songs.length) + 1
+        if (number > songs.length || number < 1) return interaction.editReply(`invalid number, must be between 1 and ${songs.length}`);
+        let song = songs[number - 1];
         const isYTLink = song.link.startsWith("https://youtu.be/");
         const embed = new EmbedBuilder()
             .setTitle(song.title)
             .setURL(song.link)
-            .setDescription(`BPM: ${song.bpm}, Vocals: ${song.vocals}`)
+            .setDescription(`BPM: ${song.bpm}, Vocals: ${song.vocals}\nNumber ${number} of ${songs.length} (Sorted by BPM)`)
         if (isYTLink) embed.setImage(`https://img.youtube.com/vi/${song.link.replace("https://youtu.be/", "")}/0.jpg`);
         await interaction.editReply({ embeds: [embed] });
     },
