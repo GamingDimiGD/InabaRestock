55f12f0a6f3eafbbc57fef49a3f3073127893747
GamingDimiGD <gamingdimigd@gmail.com>
2026-08-14 17:43:30 +0300

    It's the pfp's issue not InabaRestock's lmao

--- commands/fun/boil.js
+++ commands/fun/boil.js
@@ -18,17 +18,19 @@ module.exports = {
         const { boilCD: commandCooldown, boilURL } = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
         if (!boilURL) return await interaction.reply('boil url is not set in `config.json`, pls tell dimi about this');
         if (!interaction.options.getMentionable('user').user) return await interaction.reply('invite me to this server pls otherwise the command won\'t work for some reason');
-        const pfp = interaction.options.getMentionable('user').user.displayAvatarURL({ format: 'png' });
-        if(!pfp) return await interaction.reply('user not found!');
+        let pfp = interaction.options.getMentionable('user').user.displayAvatarURL({ format: 'png' });
+        if (!pfp) return await interaction.reply('user not found!');
+        if (pfp.endsWith('.webp')) pfp = pfp.replace('.webp', '.png');
         if (Date.now() - lastBoil < commandCooldown) return await interaction.reply('i need a break bro my cpu\'s getting fried, i\'ll be back <t:' + Math.floor((lastBoil + commandCooldown) / 1e3) + ':R>');
         lastBoil = Date.now();
         await interaction.reply('hold on a sec, im boiling...');
-        execFile('ffmpeg', [
+        console.log('[boil] Boiling ' + pfp);
+        await execFile('ffmpeg', [
             '-i', boilURL,
             '-i', pfp,
 
             '-filter_complex',
-            '[1:v]scale=300:300,colorchannelmixer=aa=0.75[img];' +
+            '[1:v]scale=300:300,colorchannelmixer=aa=0.5[img];' +
             '[0:v][img]overlay=' +
             '(main_w-overlay_w)/2:' +
             '(main_h-overlay_h)/2:' +
@@ -43,10 +45,11 @@ module.exports = {
             'output.mp4'
         ], async (error) => {
             if (error) {
+                await interaction.editReply({ content: 'error: ' + error.slice(0, 100) });
                 return console.error(error);
-                // await interaction.editReply({ content: 'error: ' + error.slice(0, 100) });
             }
-            await interaction.editReply({ files: [new AttachmentBuilder('output.mp4', { name: 'boil.mp4' })] });
+            console.log('[boil] Boiled!');
+            await interaction.editReply({ content: 'boiled <@' + interaction.options.getMentionable('user').user + '>', files: [new AttachmentBuilder('output.mp4', { name: 'boil.mp4' })] });
         })
     }
 };
