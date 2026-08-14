093b2e007aa7a98374ee6a80672f5843b4a4054a
GamingDimiGD <gamingdimigd@gmail.com>
2026-08-14 17:01:39 +0300

    Missing Semicolon

--- commands/fun/boil.js
+++ commands/fun/boil.js
@@ -28,7 +28,7 @@ module.exports = {
             '-i', pfp,
 
             '-filter_complex',
-            '[1:v]scale=300:300,colorchannelmixer=aa=0.75[img]' +
+            '[1:v]scale=300:300,colorchannelmixer=aa=0.75[img];' +
             '[0:v][img]overlay=' +
             '(main_w-overlay_w)/2:' +
             '(main_h-overlay_h)/2:' +
