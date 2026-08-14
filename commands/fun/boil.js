e323ecb08ee69254564285021ac9c935391544b2
GamingDimiGD <gamingdimigd@gmail.com>
2026-08-14 15:03:15 +0300

    15 minute cooldown

--- commands/fun/boil.js
+++ commands/fun/boil.js
@@ -1,6 +1,9 @@
 const { SlashCommandBuilder, AttachmentBuilder } = require("discord.js"),
     { execFile } = require("child_process"),
-    boilURL = 'https://cdn.discordapp.com/attachments/1414149221665214496/1531359849344929975/20260727_135620.mp4?ex=6a7fffbf&is=6a7eae3f&hm=d8e7cde34fc05bd52808f8af39f4d0187da24ce425694d844b9b114833c005a0&'
+    boilURL = 'https://cdn.discordapp.com/attachments/1414149221665214496/1531359849344929975/20260727_135620.mp4?ex=6a7fffbf&is=6a7eae3f&hm=d8e7cde34fc05bd52808f8af39f4d0187da24ce425694d844b9b114833c005a0&',
+    commandCooldown = 1e3 * 60 * 15;
+
+let lastBoil = 0;
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,6 +17,8 @@ module.exports = {
     ,
     async execute(interaction) {
         const pfp = interaction.options.getMentionable('user').user.displayAvatarURL({ format: 'png' });
+        if (Date.now() - lastBoil < commandCooldown) return await interaction.reply('i need a break bro my cpu\'s getting fried, i\'ll be back <t:' + Math.floor((lastBoil + commandCooldown) / 1e3) + ':R>');
+        lastBoil = Date.now();
         await interaction.reply('hold on a sec, im boiling...');
         execFile('ffmpeg', [
             '-i', boilURL,
