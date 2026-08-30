96de1974ee03c5316b6388df826c50c72623ef08
GamingDimiGD <gamingdimigd@gmail.com>
2026-08-30 09:26:59 +0800

    `/contributions`

---
+++ commands/misc/contributions.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+
+const contributionList = {
+    "658854081590198272": "Helped hosting the `/boil` command's API"
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("contributions")
+        .setDescription("Check everyone who contributed to this bot!")
+    ,
+    async execute(interaction) {
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle("Contributors")
+                    .setDescription(
+                        await Promise.all(
+                            Object.keys(contributionList).map(async (id) => {
+                                const user = interaction.client.users.cache.get(id) || await interaction.client.users.fetch(id);
+                                return `${user.username}: ${contributionList[id]}`;
+                            })
+                        )
+                    )
+            ]
+        })
+    },
+};
